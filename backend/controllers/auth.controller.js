7c658a5f36eeba4ba4d8792b2212e6a19c5aaa46
night-dreamer <charon.deadpan@gmail.com>
2025-05-19 09:11:16 +0800

    added get authenticated user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,29 +1,32 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { generateAccessToken, generateRefreshToken } from "../libs/utils/generateTokens.js";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../libs/utils/generateTokens.js";
 
 dotenv.config();
 
 export const signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    
-    if(!name || !email || !password) {
+
+    if (!name || !email || !password) {
       return res.status(400).json({ error: "All fields are required" });
     }
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if(!emailRegex.test(email)) {
+    if (!emailRegex.test(email)) {
       return res.status(400).json({ error: "Invalid email format" });
     }
 
     const existingUser = await User.findOne({ email }).lean();
-    if(existingUser) {
+    if (existingUser) {
       return res.status(400).json({ error: "User already exist" });
     }
 
-    const newUser = new User({ name, email, password, });
+    const newUser = new User({ name, email, password });
 
     await Promise.all([
       generateAccessToken(newUser._id, res),
@@ -31,29 +34,28 @@ export const signup = async (req, res) => {
     ]);
     await newUser.save();
 
-    res.status(201).json({...newUser._doc, password: undefined});
+    res.status(201).json({ ...newUser._doc, password: undefined });
   } catch (error) {
     console.error(`Error in signup controller: ${error.message}`);
     res.status(500).json({ error: "Internal server error" });
   }
-}
+};
 
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-
-    if(!email || !password) {
+    if (!email || !password) {
       return res.status(400).json({ error: "All fields are required" });
     }
 
     const user = await User.findOne({ email });
-    if(!user) {
+    if (!user) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
 
     const validPassword = await user.comparePassword(password);
-    if(!validPassword) {
+    if (!validPassword) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
 
@@ -67,7 +69,7 @@ export const login = async (req, res) => {
     console.error(`Error in login controller: ${error.message}`);
     res.status(500).json({ error: "Internal server error" });
   }
-}
+};
 
 export const logout = async (req, res) => {
   try {
@@ -78,14 +80,14 @@ export const logout = async (req, res) => {
     console.error(`Error in logout controller: ${error.message}`);
     res.status(500).json({ error: "Internal server error" });
   }
-}
+};
 
 export const refreshToken = async (req, res) => {
   try {
     const token = req.cookies.refreshToken;
-    if(!token) {
+    if (!token) {
       return res.status(404).json({ error: "No refresh token provided" });
-    } 
+    }
 
     const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
 
@@ -95,14 +97,13 @@ export const refreshToken = async (req, res) => {
     console.error(`Error in refreshToken controller: ${error.message}`);
     res.status(500).json({ error: "Internal server error" });
   }
-}
+};
 
-// todo: implement accessRoute
-// export const getProfile = async (req, res) => {
-//   try {
-
-//   } catch (error) {
-//     console.error(`Error in getProfile controller: ${error.message}`);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// }
+export const getProfile = async (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.error(`Error in getProfile controller: ${error.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { login, logout, refreshToken, signup } from "../controllers/auth.controller.js";
+import { accessRoute } from "../middleware/auth.middleware.js";
+import {
+  getProfile,
+  login,
+  logout,
+  refreshToken,
+  signup,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -7,6 +14,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
-// router.get("/profile", getProfile);
+router.get("/profile", accessRoute, getProfile);
 
 export default router;
